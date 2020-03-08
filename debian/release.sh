bbdececac1b3f151701da990438b092777f690a1
David Lechner <david@pybricks.com>
2020-03-08 16:37:21 -0500

    debian: maintainer release script

---
+++ debian/release.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Maintainer script for publishing releases.
+
+set -e
+
+source=$(dpkg-parsechangelog -S Source)
+version=$(dpkg-parsechangelog -S Version)
+
+debuild -S
+
+debuild -- clean
+
+dput ppa:pybricks/ppa ../${source}_${version}_source.changes
+
+gbp buildpackage --git-tag-only
